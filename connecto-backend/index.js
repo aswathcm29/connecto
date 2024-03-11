81a1a32ee016aae5b39bfbbd00fef404fc39db6d
Cibiyanna <pkcibiyanna@gmail.com>
2024-03-11 14:56:09 +0530

    changed the database

--- connecto-backend/index.js
+++ connecto-backend/index.js
@@ -3,15 +3,15 @@ const express = require('express')
 const app = express()
 const userRouter = require('./routes/userRoutes')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const connection = require('./db')
 
 
 connection()
+app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.json())
-
 app.use('/api/users',userRouter)
-
 
 app.listen(process.env.PORT,()=>{
     console.log('listening on port',process.env.PORT)
