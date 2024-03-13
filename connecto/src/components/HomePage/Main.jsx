1ec989f5dff6fae50bfa0224e1b497735fda8964
Cibiyanna <pkcibiyanna@gmail.com>
2024-03-13 10:18:37 +0530

    add post

--- connecto/src/components/HomePage/Main.jsx
+++ connecto/src/components/HomePage/Main.jsx
@@ -9,16 +9,16 @@ const Main = () => {
   const [addPost,setAddPost] = useState(false)
   return (
     <>
-    <div className='bg-zinc-900 relative'>
-        <div className="flex w-[80%] mx-auto items-center justify-around ">
-            <Left/>
-            <Posts addPost={addPost} setAddPost={setAddPost}/>
-            <Right/>
-        </div>
-        <div className='absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 '>
-          <PopupPost addPost={addPost} setAddPost={setAddPost} />
-        </div>
-    </div>
+      <div className='bg-zinc-900 relative'>
+          <div className="flex w-[80%] mx-auto items-center justify-around ">
+              <Left/>
+              <Posts addPost={addPost} setAddPost={setAddPost}/>
+              <Right/>
+          </div>
+          <div className='absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 '>
+            <PopupPost addPost={addPost} setAddPost={setAddPost} />
+          </div>
+      </div>
     </>
   )
 }
